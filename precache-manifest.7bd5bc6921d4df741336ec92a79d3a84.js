03f73791aa758ae05f8e091c540c223f660feb3d
Danzel Rana <danzelrana@gmail.com>
2020-02-16 18:15:48 -0800

    Updates

---
+++ precache-manifest.7bd5bc6921d4df741336ec92a79d3a84.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d384426931bb1491af6ad7fee16ee59c",
+    "url": "/anime-watch/index.html"
+  },
+  {
+    "revision": "e71f5be7a979e7b4fc72",
+    "url": "/anime-watch/static/css/2.083d79f4.chunk.css"
+  },
+  {
+    "revision": "06bd873cbd1ea4b4c24b",
+    "url": "/anime-watch/static/css/main.69bbf47f.chunk.css"
+  },
+  {
+    "revision": "e71f5be7a979e7b4fc72",
+    "url": "/anime-watch/static/js/2.b3d43951.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/anime-watch/static/js/2.b3d43951.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06bd873cbd1ea4b4c24b",
+    "url": "/anime-watch/static/js/main.f9ac620e.chunk.js"
+  },
+  {
+    "revision": "3aabdb6cc8fb40c4e72d",
+    "url": "/anime-watch/static/js/runtime-main.02244aaa.js"
+  },
+  {
+    "revision": "fba7636ece9e0b5bd76e97b97f725089",
+    "url": "/anime-watch/static/media/register-gif.fba7636e.gif"
+  },
+  {
+    "revision": "8feb230c03748a9842068d9581473910",
+    "url": "/anime-watch/static/media/signin-gif.8feb230c.gif"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/anime-watch/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/anime-watch/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/anime-watch/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/anime-watch/static/media/slick.f97e3bbf.svg"
+  }
+]);
